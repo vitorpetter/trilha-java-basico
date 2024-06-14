af844edff395537974ae729129c2066d21516792
vitorpetter <vitorhmpetter@gmail.com>
2024-06-13 23:14:18 -0300

    entrega desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try (Scanner sc = new Scanner(System.in)) {
+            sc.useLocale(Locale.US);
+            
+            System.out.println("Por favor, digite o número da Conta!");
+            int  conta = sc.nextInt();
+            sc.nextLine(); // Consumir a nova linha deixada pelo nextInt()
+            
+            System.out.println("Agora, digite o número da Agência!");
+            String agencia = sc.nextLine();
+            
+            System.out.println("Agora, digite o seu Nome Completo!");
+            String nomeCliente = sc.nextLine();
+            
+            System.out.println("Agora, digite o saldo atual!");
+            float saldo = sc.nextFloat();
+            
+            System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo de R$ "+saldo+" já esta disponível para saque");
+        }
     }
 }
